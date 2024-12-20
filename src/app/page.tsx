a0cd334b381839a4362cf54c48751c79589dd812
ShumailaKhan12 <shumaila1641@gmail.com>
2024-12-20 17:25:26 +0530

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/Components/navbar/navbar";
 import MatchCards from "./match-details/page";
 
 export default function Home() {
   return (
 <div>
+  <Navbar/>
   <MatchCards/>
 </div>
   );

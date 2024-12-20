56a31c94a0e40dfe26fa48e7cb453259f992cb73
himanshuverma13 <himanshu.verma@skillintelligence.tech>
2024-12-20 17:24:08 +0530

    news 20/12/24

--- src/APIs/api.tsx
+++ src/APIs/api.tsx
@@ -1,11 +1,20 @@
+import axios from "axios"
 import axiosInstance from "./axiosInterceptor"
 const URL:any = process.env.NEXT_PUBLIC_MATCH_CARD_API
-console.log('URL: ', URL);
 
 export const GetMatchCardAPI = async()=>{
     try {
         let response = await axiosInstance.get(`${URL}/matches`)
         return  response?.data 
+    } catch (error) {
+        console.log('error: ', error);
+    }
+}
+
+export const GetNewsAPI = async()=>{
+    try {
+        let response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=10308ea6f51d479491f051fdd598aa9b')
+        return  response?.data?.articles 
     } catch (error) {
         console.log('error: ', error);
     }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import News from "@/Components/News/news";
 import MatchCards from "./match-details/page";
 
 export default function Home() {
   return (
 <div>
   <MatchCards/>
+  <News/>
 </div>
   );
 }
